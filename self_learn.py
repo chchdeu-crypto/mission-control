5ce71e2a558e147601507dca886275ccc01f285a
chaim <chchdeu@gmail.com>
2026-06-18 21:59:30 +0300

    added bin

---
+++ self_learn.py
@@ -0,0 +1,20 @@
+#understandig questions
+#12=1100
+#55=110111
+#29=11101
+#-10=11110110
+
+#2=2 בחזקת 36
+#five_str = "5"
+#five_int = 5
+#print(bin(five_str))
+#print(bin(five_int))
+#they are not the same because one is str 
+#they can not be compard
+#2 practice
+gad = True
+chaim = False
+print(bin(gad))
+print(bin(chaim))
+print(gad == chaim)
+print(gad < chaim)
